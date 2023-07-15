0415a6c34b760072ebc8e68f5f081bbfadc657fd
Giordano Bruno <giordanobrunotj@hotmail.com>
2023-07-15 15:08:21 +0100

    time is not decreasing on timer method

--- clock.py
+++ clock.py
@@ -1,7 +1,27 @@
 class clock:
 
-    def timer(time):
-        time.split(":")
+    def timer(self, timeStr):
+        time = timeStr.split(":")
+        time[0] = int(time[0])
+        time[1] = int(time[1])
+        time[2] = int(time[2])
+
+        if(time[0]!=0 or time[1]!=0 or time[0]!=0):
+        
+            if(time[1]==0 and time[0]!=0):
+                time[0] = time[0]- 1
+                time[1]=60
+
+            if(time[2]==0 and time[1]!=0):
+                time[1] = time[1] - 1
+                time[0] = 60
+
+            time[2] -=1
+        print(time[0],":",time[1],":",time[0],"\r")
+
+            
+            
+      
 
     def stopWatch():
         pass
@@ -11,7 +31,7 @@ obj = clock()
 option = int(input("1 - Timer\n2 - Stop Watch "))
 while(True):
     if(option==1):
-
+        
         obj.timer(input("Enter the time in this format 00:00:00 "))
     elif(option==2):
         obj.stopWatch()
